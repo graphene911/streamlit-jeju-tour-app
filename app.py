2eef7cc27d94e202c6e2479cbd637fabad347624
graphene911 <graphene911@gmail.com>
2023-01-27 16:42:43 +0900

    add

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 from app_home import run_home
 from app_etc import run_etc
 from app_report import run_report
-
+from app_report_nh import run_report_nh
 
 def main() :
     
     st.set_page_config(layout="wide")
 
-    menu = ['Error Device(Sunjin)', 'Error Device(All)' ,'Myfeed Report', ]
+    menu = ['Error Device(선진)', 'Error Device(전체)' ,'Myfeed Report(선진)','Myfeed Report(서울축산농협)' ]
     choice = st.sidebar.selectbox('메뉴 선택', menu)
     
     if choice == menu[0] :
@@ -17,6 +17,8 @@ def main() :
         run_etc()
     elif choice == menu[2] :
         run_report()
+    elif choice == menu[3] :
+        run_report_nh()
 
 
 if __name__ == '__main__' :
