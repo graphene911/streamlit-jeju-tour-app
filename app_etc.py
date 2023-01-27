3ebe2a87ad7ca625260a0e3dd9c6a60959e9519c
graphene911 <graphene911@gmail.com>
2023-01-27 16:35:55 +0900

    add

---
+++ app_etc.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+
+def run_etc() :
+    
+    url = 'http://topping.io:8000/API/silos/error_silo?user_seq=8'
+    
+    response = requests.get(url).json()
+
+    df = pd.DataFrame.from_dict(response)
+    
+    st.title('Error Device (Sunjin)')
+    st.subheader('(Count :' + ' ' + str(df['sn'].count()) + ')')
+
+    st.image('data/#fc6858_line.png', width = 1753)
+    
+    st._legacy_table(df)
+
---
+++ app_home.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import requests
+
+
+def run_home() :
+    
+    url = 'http://topping.io:8000/API/silos/error_silo?user_seq=30'
+    response = requests.get(url).json()
+
+    df = pd.DataFrame.from_dict(response)
+    
+    st.title('Error Device (Sunjin)')
+    st.subheader('(Count :' + ' ' + str(df['sn'].count()) + ')')
+
+    st.image('data/#fc6858_line.png', width = 1753)
+    
+    st._legacy_table(df)
+
