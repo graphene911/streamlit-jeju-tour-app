c4dc3a6fffa01476f25cc8ae63d319393603c22d
graphene911 <graphene911@gmail.com>
2023-02-28 09:54:37 +0900

    add

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from app_home import run_home
 from app_etc import run_etc
 from app_report import run_report
 from app_report_nh import run_report_nh
-
+from app_mqtt import run_mqtt
 def main() :
     
     st.set_page_config(layout="wide")
 
-    menu = ['Error Device(선진)', 'Error Device(선진제외전체)' ,'Myfeed Report(선진)', 'Myfeed Report(서울축산농협)' ]
+    menu = ['Error Device(선진)', 'Error Device(선진제외전체)' ,'Myfeed Report(선진)', 'Myfeed Report(서울축산농협)', 'mqtt' ]
     choice = st.sidebar.selectbox('메뉴 선택', menu)
     
     if choice == menu[0] :
@@ -19,6 +19,8 @@ def main() :
         run_report()
     elif choice == menu[3] :
         run_report_nh()
+    elif choice == menu[4] :
+        run_mqtt()
 
 
 if __name__ == '__main__' :
---
+++ app_mqtt.py
@@ -0,0 +1,28 @@
+import streamlit as st
+import plotly.express as px
+import numpy as np
+import pandas as pd
+import requests
+import plotly.express as px
+import json
+from bs4 import BeautifulSoup
+
+
+def run_mqtt() :
+    url = 'https://www.aimbelab.com/mqtt.php'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    # html_code = soup.prettify()
+    program_names = soup.select('td > p > a')
+    for tag in program_names:
+	    print(tag.get_text())
+        # print(tag_text)
+    # print(html_code)
+    # articles = soup.find_all('div', {'class': 'cluster_text'})
+
+    # for article in articles:
+    #     title = article.find('SN').get_text().strip()
+    #     link = article.find('SN')['href']
+    #     print(title)
+    #     print(link)
+    # return tag_text
