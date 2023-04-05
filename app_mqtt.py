ab348f4e6f09035322c5a4b7e30bdcc39aa9520c
graphene911 <graphene911@gmail.com>
2023-04-05 15:20:51 +0900

    add

--- app_mqtt.py
+++ app_mqtt.py
@@ -6,7 +6,7 @@ import requests
 # run_mqtt 함수 정의
 def run_mqtt(silo_sn, start_date, end_date):
     
-    
+
     url = 'http://20.214.200.233:8000/API/silos/get_silo_list2?silo_sn={}&start_date={}&end_date={}'.format(silo_sn, start_date, end_date)
     response = requests.get(url)
     response_json = response.json() # JSON으로 변환
@@ -16,7 +16,7 @@ def run_mqtt(silo_sn, start_date, end_date):
         st.download_button(
                     label="Download CSV File",
                     data=file,
-                    file_name="DATA_LIST.csv",
+                    file_name="data_list.csv",
                     mime="application/octet-stream")
     df = df.sort_values(by='RecordTime', ascending=False)
     st._legacy_table(df)
