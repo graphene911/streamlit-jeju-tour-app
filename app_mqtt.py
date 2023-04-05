c58aca9a3223b9ae20db87f51ce5e514bc1b98f5
graphene911 <graphene911@gmail.com>
2023-04-05 15:09:31 +0900

    add

--- app_mqtt.py
+++ app_mqtt.py
@@ -17,6 +17,7 @@ def run_mqtt(silo_sn, start_date, end_date):
                     data=file,
                     file_name="DATA_LIST.csv",
                     mime="application/octet-stream")
+    df = df.sort_values(by='RecordTime', ascending=False)
     st._legacy_table(df)
 
 
